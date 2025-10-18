285ee8d4c710c1a28a9e38f940810c499b751a1c
Toby Crisford <tobycrisford@gmail.com>
2025-10-18 17:49:40 +0100

    Finalizing and testing outcome polynomial class

--- equilibrium_calc.py
+++ equilibrium_calc.py
@@ -7,6 +7,7 @@ import numpy as np
 class OutcomePolynomialException(Exception):
     pass
 
+
 class OutcomePolynomial:
     def __init__(self, coefs: dict[tuple[bool, ...], float]):
         self.n_probs = -1
@@ -22,23 +23,29 @@ class OutcomePolynomial:
 
         self.outcome_to_index = {}
         self.index_to_outcome = []
-        self.coef_array = np.zeros(2**(self.n_probs))
-        self.outcome_array = np.zeros((2**(self.n_probs), self.n_probs))
-        for i, outcome in enumerate(itertools.product([False, True], repeat=self.n_probs)):
+        self.coef_array = np.zeros(2 ** (self.n_probs))
+        self.outcome_array = np.zeros((2 ** (self.n_probs), self.n_probs))
+        for i, outcome in enumerate(
+            itertools.product([False, True], repeat=self.n_probs)
+        ):
             self.index_to_outcome.append(outcome)
             self.outcome_to_index[outcome] = i
-            self.outcome_array[i,:] = np.array(outcome)
+            self.outcome_array[i, :] = np.array(outcome)
             if outcome in coefs:
                 self.coef_array[i] = coefs[outcome]
 
         self.terms = coefs
 
-    def _poly_eval(self, probs: np.ndarray, outcome_array: np.ndarray, coef_array: np.ndarray) -> np.ndarray:
+    def _poly_eval(
+        self, probs: np.ndarray, outcome_array: np.ndarray, coef_array: np.ndarray
+    ) -> float:
         n_probs = len(probs)
 
-        relevant_probs = probs.reshape((1, n_probs)) * outcome_array + (1 - probs).reshape((1, n_probs)) * (1 - outcome_array)
+        relevant_probs = probs.reshape((1, n_probs)) * outcome_array + (
+            1 - probs
+        ).reshape((1, n_probs)) * (1 - outcome_array)
         return np.sum(np.prod(relevant_probs, axis=1) * coef_array)
-    
+
     def eval(self, probs: np.ndarray) -> float:
         if len(probs) != self.n_probs:
             raise OutcomePolynomialException("Supplied probabilities have wrong length")
@@ -64,7 +71,18 @@ class OutcomePolynomial:
             mask[i] = True
 
         return np.array(partial_derivs)
-            
+
+    def __add__(self, other_polynomial):
+        summed_coefs = {}
+        for terms in (self.terms, other_polynomial.terms):
+            for k in terms:
+                if k not in summed_coefs:
+                    summed_coefs = terms[k]
+                summed_coefs += terms[k]
+
+        return OutcomePolynomial(summed_coefs)
+
+
 def create_value_polynomial(
     n_probs: int,
     value_function: Callable[[int], float],
@@ -78,3 +96,18 @@ def create_value_polynomial(
         coefs[outcome] = coef
 
     return OutcomePolynomial(coefs)
+
+
+def test_value_fn(n_eaten: int) -> float:
+    if n_eaten == 0:
+        return 6
+    elif n_eaten == 1:
+        return -1 * ((3 * 6) / 4)
+    elif n_eaten == 2:
+        return -1 * ((2 * 6) / 4)
+    elif n_eaten == 3:
+        return -1 * (6 / 4)
+    elif n_eaten == 4:
+        return 0
+    else:
+        raise NotImplementedError()

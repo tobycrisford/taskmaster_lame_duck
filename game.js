94a0621ba5b0927c7f57d243a976a43d7f21d201
Toby Crisford <tobycrisford@gmail.com>
2025-11-23 09:33:47 +0000

    Adding value selection sliders

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ const CHARACTERS = ['Ania Magliano', 'Maisie Adam', 'Phil Ellis', 'Reece Shearsm
 
 const SCREENS = {CHARACTER_SELECT: 0, VALUE_SELECT: 1, DUCK_CHOICE: 2, RESULTS: 3};
 
+const VALUE_MAX = 10;
+const VALUE_DEFAULT = 0;
+
 let screen = SCREENS.CHARACTER_SELECT;
 let player_character = null;
 
@@ -33,10 +36,44 @@ function draw_intro_screen(box) {
     }
 }
 
+function create_value_input(character, description) {
+    const input_container = document.createElement("div");
+    input_container.setAttribute("class", "value_input_container");
+    input_container.textContent = description;
+    const value_input = document.createElement("input");
+    value_input.setAttribute("type", "range");
+    value_input.setAttribute("min", "0");
+    value_input.setAttribute("max", VALUE_MAX.toString());
+    value_input.setAttribute("value", VALUE_DEFAULT.toString());
+    value_input.setAttribute("class", "value_slider");
+    input_container.appendChild(value_input);
+    const current_selection = document.createElement("span");
+    current_selection.textContent = VALUE_DEFAULT.toString();
+    input_container.appendChild(current_selection);
+    value_input.oninput = () => {current_selection.textContent = value_input.value};
+    return input_container;
+}
+
 function draw_value_screen(box) {
     box.innerHTML = "";
-    const text = create_text("Your selected character is " + player_character);
-    box.appendChild(text);
+    let text = [];
+    text.push("Your selected character is " + player_character);
+    text.push("How many points would each character be willing to give up in exchange for £25?");
+    text.push("Include your own tradeoff.");
+    text.push("Each player's points/cash tradeoff is common knowledge for all players.");
+    for (const line of text) {
+        const textbox = create_text(line);
+        box.appendChild(textbox);
+    }
+    let val_input = create_value_input(player_character, player_character + ' (You)');
+    box.appendChild(val_input);
+    for (const character of CHARACTERS) {
+        if (character === player_character) {
+            continue;
+        }
+        val_input = create_value_input(character, character);
+        box.appendChild(val_input);
+    }
 }
 
 function draw() {
